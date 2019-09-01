2b994c5b079e2d43aab95f744d123aa463275afb
Rahul <rahul.brahmal@me.com>
2019-09-01 15:43:37 -0400

    Clean push for web.

--- services/web/src/App.js
+++ services/web/src/App.js
@@ -1,35 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {
-  Container, Row, Col, Navbar, NavbarBrand, Nav, NavItem, NavLink, Card, CardBody, CardTitle, CardSubtitle, CardText, Button
-} from 'reactstrap';
 
-
-class App extends Component {
-    state = {
-        contacts: []
-    }
-
-    // componentDidMount() {
-    //
-    // }
-
-    render() {
-        return (
-            <Row>
-                <div align='center'>
-                    <h1>
-                        Welcome to HangMan
-                    </h1>
-                </div>
-            </Row>
-
-            <Row>
-                <Button color="primary">primary</Button>
-            </Row>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 
 export default App;
