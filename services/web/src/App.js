a9d6d264fa465d66d328d6e514568b57e894c4a0
Rahul <rahul.brahmal@me.com>
2019-09-01 11:23:58 -0400

    Hangman dev in app.js created.

--- services/web/src/App.js
+++ services/web/src/App.js
@@ -1,22 +1,37 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  Container, Row, Col, Navbar, NavbarBrand, Nav, NavItem, NavLink, Card, CardBody, CardTitle, CardSubtitle, CardText, Button
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-        <h1>
-            HangMan!
-        </h1>
+class App extends Component {
+    state = {
+        contacts: []
+    }
 
-        <p2>
-            By: Rahul & Richard
-        </p2>
-      </header>
-    </div>
-  );
+    componentDidMount() {
+
+    }
+
+    render() {
+
+        return (
+            <Row>
+                <div align='center'>
+                    <h1>
+                        Welcome to HangMan
+                    </h1>
+                </div>
+            </Row>
+
+            // <Row>
+            //     <Button color="danger"> Single Player </Button>
+            //     <Button color="danger"> Multi Player </Button>
+            // </Row>
+        );
+    }
 }
+
 
 export default App;
